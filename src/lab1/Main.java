1b5c5e2690b57a992ed33ba0a5bae3e25a8eb726
mennto <tt_mge@ukr.net>
2023-03-28 23:03:57 +0300

    lab2

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,8 +1,15 @@
 package lab1;
 
+import lab2.Bank;
+import lab2.Thread1;
+import lab2.Thread2;
+
 public class Main {
-    public static void main(String[] args) throws IncorrectSalaryException, FieldLengthLimitException {
-        Employee rabotyaga = new Employee();
-        rabotyaga.setName("asfjuisafhiaushfiuahf");
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        Thread1 t1 = new Thread1(bank);
+        Thread2 t2 = new Thread2(bank);
+        t1.run();
+        t2.run();
     }
 }
