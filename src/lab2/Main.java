88e6281cb51b450a4e7e87a40562171e56e71ac4
mennto <tt_mge@ukr.net>
2023-03-29 19:51:39 +0300

    lab3

---
+++ src/lab2/Main.java
@@ -0,0 +1,15 @@
+package lab2;
+
+import lab2.Bank;
+import lab2.Thread1;
+import lab2.Thread2;
+
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        Thread1 t1 = new Thread1(bank);
+        Thread2 t2 = new Thread2(bank);
+        t1.run();
+        t2.run();
+    }
+}
